b3338952ed32688d0b145cd244672bd191ba448c
Pandizzler <94189480+ChinesePrince07@users.noreply.github.com>
2026-02-07 22:33:31 -0500

    Add SETUP option to settings menu for WiFi config
    - SETUP broadcasts captive portal for first-time WiFi setup
    - Connect to "CALC" WiFi, open browser, enter credentials
    - Restored TI32 program name

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -719,7 +719,7 @@ void send() {
 }
 
 void _sendLauncher() {
-  sendProgramVariable("ANDYGPT", __launcher_var, __launcher_var_len);
+  sendProgramVariable("TI32", __launcher_var, __launcher_var_len);
 }
 
 void launcher() {
